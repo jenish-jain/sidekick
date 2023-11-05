89af46c19db61eb9c53092a512535d8899cb29b6
jenish-jain <jenishjain@rocketmail.com>
2023-11-05 12:06:43 +0530

    #minor | adding copy to clipboard functionality

--- internal/twoFA/sub_commands.go
+++ internal/twoFA/sub_commands.go
@@ -3,6 +3,7 @@ package twoFA
 import "github.com/jenish-jain/sidekick/internal"
 
 var (
-	Add  = internal.NewBooleanFlag("add", "a", false, "add a key")
+	Add  = internal.NewBooleanFlag("add", "a", false, "add a key \t\t\t[1 argument <name>  required!]")
 	List = internal.NewBooleanFlag("list", "l", false, "lists all keys added")
+	Clip = internal.NewBooleanFlag("clip", "c", false, "copy code to the clipboard \t[1 argument <name>  required!]")
 )
